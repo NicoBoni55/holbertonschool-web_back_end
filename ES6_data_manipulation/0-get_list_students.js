afde4003f17418cf227d1bf47ade64639df33a12
NicoBoni55 <bonifrank55@gmail.com>
2024-07-26 11:26:23 -0400

    0-get_list_students

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-  const Obj1 = { id: 1, FirstName: 'Guillaume', location: 'San Francisco' };
-  const Obj2 = { id: 2, FirstName: 'James', location: 'Columbia' };
-  const Obj3 = { id: 5, FirstName: 'Serena', location: 'San Francisco' };
+  const Obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const Obj2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const Obj3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
   const NewArray = [Obj1, Obj2, Obj3];
   return (NewArray);
 }
