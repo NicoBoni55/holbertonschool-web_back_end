3a8abbd56c3f2079a5b8ae3988457d19aa16a60f
NicoBoni55 <bonifrank55@gmail.com>
2024-07-26 18:44:51 -0400

    9-groceries_list

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  const elements = new Map();
+
+  elements.set('Apples', 10);
+  elements.set('Tomatoes', 10);
+  elements.set('Pasta', 1);
+  elements.set('Rice', 1);
+  elements.set('Banana', 5);
+
+  return elements;
+}
