b7856e44146dc6e9c5c1a3d7fbdc5c1ff8bddc5f
NicoBoni55 <bonifrank55@gmail.com>
2024-07-27 17:26:14 -0400

    2-then

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,14 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then((onfulfilled) => {
-    if (onfulfilled) {
+  return promise
+    .then(() => {
       console.log('Got a response from the API');
       return ({
         status: 200,
         body: 'Success',
       });
-    }
-    console.log('Got a response from the API');
-    return new Error();
-  });
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
