4fa6f00a2eb2cf2c89418e9c8d60330b428d3ee9
NicoBoni55 <bonifrank55@gmail.com>
2024-07-26 22:54:58 -0400

    8-clean_set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 export default function cleanSet(set, startString) {
-  if (startString) {
-    const result = [];
-    for (const item of set) {
-      if (item.startsWith(startString)) {
-        const newItem = item.slice(startString.length);
-        result.push(newItem);
-      }
-    }
-    return result.join('-');
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
   }
-  return '';
+  const result = [];
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      const newItem = item.slice(startString.length);
+      result.push(newItem);
+    }
+  }
+  return result.join('-');
 }
