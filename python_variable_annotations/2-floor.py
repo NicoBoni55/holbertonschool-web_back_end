40b56b017c3f9bdee0dc562e004ead3a4ea74e50
NicoBoni55 <bonifrank55@gmail.com>
2024-08-13 14:23:27 -0400

    2-floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function that returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    return the floor of the float
+
+    Parameters:
+    n (float)
+
+    return: int(n)
+    """
+    return int(n)
