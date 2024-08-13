6651e4a1d09a28124baa20d53825124ee40f9d35
NicoBoni55 <bonifrank55@gmail.com>
2024-08-13 15:29:25 -0400

    8-make_multiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+function that returns a function that multiplies a float by a multiplier
+"""
+
+import typing
+
+
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    """
+    return a function that multiplies a float by a multiplier
+
+    parameters:
+    multiplier (float)
+
+    return: Callable[[float], float]
+    """
+    def mul(n: float) -> float:
+        """
+        return the multiplication of n by multiplier
+
+        parameters:
+        n (float)
+
+        return: float
+        """
+        return n * multiplier
+    return mul
