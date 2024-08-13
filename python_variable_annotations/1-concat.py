801efbbc4035dd760385fe4284af77bc3d120100
NicoBoni55 <bonifrank55@gmail.com>
2024-08-13 14:20:46 -0400

    1-concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+function that concatenates two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenate two strings
+
+    Parameters:
+    str1 (str)
+    str2 (str)
+
+    return: str1 + str2
+    """
+    return str1 + str2
