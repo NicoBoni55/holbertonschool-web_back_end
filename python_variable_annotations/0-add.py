e4ac806b8eae69b0501d78f32d3827c190984d24
NicoBoni55 <bonifrank55@gmail.com>
2024-08-13 14:17:10 -0400

    0-add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -5,4 +5,13 @@ function that adds two floats
 
 
 def add(a: float, b: float) -> float:
+    """
+    add two floats
+
+    Parameters:
+    a (float)
+    b (float)
+
+    return: a + b
+    """
     return a + b
