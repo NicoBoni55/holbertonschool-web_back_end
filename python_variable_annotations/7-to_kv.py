ad03bc72b908014f6438f51f68a03af01269aedb
NicoBoni55 <bonifrank55@gmail.com>
2024-08-13 15:17:40 -0400

    7-to_kv

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+function that takes a float n as argument and returns a tuple:
+"""
+
+import typing
+
+
+def to_kv(k: str, n: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """
+    return a tuple with the first element str and the second float
+
+    parameters:
+    k (str)
+    n (Union[int, float])
+
+    return: Tuple[str, float]
+    """
+    return (k, pow(n, 2))
