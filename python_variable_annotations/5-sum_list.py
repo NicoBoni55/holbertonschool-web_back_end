f1d2e53469228021db730b6c85599bcbd73bd164
NicoBoni55 <bonifrank55@gmail.com>
2024-08-13 14:38:47 -0400

    5-sum_list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function that returns the sum of a list of floats
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    return the sum of the list of floats
+
+    Parameters:
+    input_list (List[float])
+
+    return: sum of the list of floats
+    """
+    return sum(input_list)
