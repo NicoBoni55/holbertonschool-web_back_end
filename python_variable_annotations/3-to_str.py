f224183eda282b83afaa458d014e62f9506e6617
NicoBoni55 <bonifrank55@gmail.com>
2024-08-13 14:28:01 -0400

    3-to_str

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -7,10 +7,10 @@ function that returns the string representation of a float
 def to_str(n: float) -> str:
     """
     return the string representation of the float
-    
+
     parameters:
     n (float)
-    
+
     return: str(n)
     """
     return str(n)
