c499a94450722a9a28fa8743d3b43b4a8318ab66
NicoBoni55 <bonifrank55@gmail.com>
2024-08-21 08:52:03 -0400

    2-hypermedia_pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -58,7 +58,7 @@ class Server:
             next_page = page + 1
         if page == 1:
             prev_page = None
-        else:    
+        else:
             prev_page = page - 1
         total_page = math.ceil(len(self.dataset()) / page_size)
         dictionary = {
@@ -71,13 +71,3 @@ class Server:
         }
 
         return dictionary
-
-server = Server()
-
-print(server.get_hyper(1, 2))
-print("---")
-print(server.get_hyper(2, 2))
-print("---")
-print(server.get_hyper(100, 3))
-print("---")
-print(server.get_hyper(3000, 100))
