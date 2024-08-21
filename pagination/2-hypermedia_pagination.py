ac1127c4e9c13767c1c85ddcef59472aab2ff7a4
NicoBoni55 <bonifrank55@gmail.com>
2024-08-21 08:46:47 -0400

    2-hypermedia_pagination

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
+Class Server
+"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    return a tuple with two index
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        return the page with the start and end index
+        """
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0
+        assert page_size > 0
+
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        return a dictionary
+        """
+        data = self.get_page(page, page_size)
+        if page + 1 == 0:
+            next_page = None
+        else:
+            next_page = page + 1
+        if page - 1 == 0:
+            prev_page = None
+        else:
+            prev_page = page - 1
+        total_page = math.ceil(len(self.dataset()) / page_size)
+        dictionary = {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_page': total_page
+        }
+
+        return dictionary
