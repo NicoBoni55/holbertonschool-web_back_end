28aa1d09804ea34a5c7cfb50f51288b7b3e46aee
NicoBoni55 <bonifrank55@gmail.com>
2024-09-20 11:42:36 -0400

    3-read_file_async.js

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  try {
+    const data = await fs.promises.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = {};
+    for (let i = 1; i < lines.length; i += 1) {
+      const student = lines[i].split(',');
+      if (student.length >= 4) {
+        const field = student[3].trim();
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(student[0].trim());
+      }
+    }
+    console.log(`Number of students: ${lines.length - 1}`);
+    for (const field in students) {
+      if (field) {
+        const names = students[field];
+        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
