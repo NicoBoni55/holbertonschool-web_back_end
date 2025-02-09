628fa3fd19ec02fe1a6cb0eb9a403f7fd403a868
NicoBoni55 <bonifrank55@gmail.com>
2025-02-09 19:38:15 -0300

    4-mru_cache.py

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''
+Basic Dictionary: Create a class MRUCache that inherits from BaseCaching
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    '''
+    Class MRUCache
+    '''
+    def __init__(self):
+        super().__init__()
+        self.order_data = []
+
+    def put(self, key, item):
+        '''
+        Add key/value pair to cache.
+        '''
+        if key is not None and item is not None:
+            if key not in self.cache_data:
+                self.order_data.append(key)
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last_key = self.order_data.pop(-2)
+            self.cache_data.pop(last_key)
+            print(f"DISCARD: {last_key}")
+
+    def get(self, key):
+        '''
+        Return value stored in `key` of cache.
+        '''
+        if key is not None and key in self.cache_data:
+            self.order_data.remove(key)
+            self.order_data.append(key)
+            return self.cache_data[key]
+        return None
