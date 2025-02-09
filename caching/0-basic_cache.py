073db6a5e1f58105e3bc8eb689dd55a5df6a61d0
NicoBoni55 <bonifrank55@gmail.com>
2025-02-09 14:11:35 -0300

    0-basic_cache

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,18 +1,29 @@
-#!/usr/bin/env python3
-"""
-create BasicCache class
-"""
+#!/usr/bin/python3
+'''
+Basic Dictionary: Create a class BasicCache that inherits from BaseCaching
+'''
 
-from base_caching import BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
+    '''
+    A basic cache.
+    put - method that adds a key/value pair to cache
+    get - method that retrieves a key/value pair from cache
+    '''
 
     def put(self, key, item):
+        '''
+        Add key/value pair to cache.
+        '''
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        '''
+        Return value stored in `key` of cache.
+        '''
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
