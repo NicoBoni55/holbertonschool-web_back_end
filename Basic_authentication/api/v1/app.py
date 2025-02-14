58153d5806e07fe2afeb08c9a58f5911404f8735
NicoBoni55 <bonifrank55@gmail.com>
2025-02-14 17:24:35 -0300

    fix

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -22,7 +22,7 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
+def unauthorized_error(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
@@ -30,4 +30,4 @@ def unauthorized(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
