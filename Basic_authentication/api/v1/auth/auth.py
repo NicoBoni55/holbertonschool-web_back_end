91c477fc2602345a5c27f01fa45d60e7b7a265f8
NicoBoni55 <bonifrank55@gmail.com>
2025-02-17 15:04:56 -0300

    2-auth

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Authentication
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """Class to manage auth
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
