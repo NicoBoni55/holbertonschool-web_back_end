0fd22fcc925ade258e331844ba425d70fa1b2ceb
NicoBoni55 <bonifrank55@gmail.com>
2025-02-19 17:12:29 -0300

    Basic decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,3 +21,19 @@ class BasicAuth(Auth):
             return None
         basic = authorization_header.split(' ')
         return basic[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ decode base64
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            encode = base64_authorization_header.encode('utf-8')
+            encode = base64.b64decode(encode)
+            decode = encode.decode('utf-8')
+            return decode
+        except Exception:
+            return None
