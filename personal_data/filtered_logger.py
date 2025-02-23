2b0c2d794fb2aa405fbeb64f04df6fb55c515987
NicoBoni55 <bonifrank55@gmail.com>
2025-02-23 12:18:56 -0300

    fix pycode

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -18,15 +18,17 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """ Filter values with filter_datum
         """
-        return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
+        message = filter_datum(self.fields, self.REDACTION,
+                               super().format(record), self.SEPARATOR)
+
+        return message
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
