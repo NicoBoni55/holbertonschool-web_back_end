1619233991acad8588c3a7e1c1bde826b7bf01ff
NicoBoni55 <bonifrank55@gmail.com>
2025-02-26 18:11:13 -0300

    Register users

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,8 +2,12 @@
 """
 App flask
 """
-from flask import Flask
+from flask import Flask, jsonify, request, abort
 import flask
+from auth import Auth
+
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -13,6 +17,20 @@ app = Flask(__name__)
 def login():
     welcome = {"message": "Bienvenue"}
     return flask.jsonify(welcome)
+
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+    except KeyError:
+        abort(400)
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
