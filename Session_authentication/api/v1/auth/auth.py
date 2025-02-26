f888f60e3abb148b765a63a26eca4f593aed7d0b
NicoBoni55 <bonifrank55@gmail.com>
2025-02-26 10:32:12 -0300

    User model

---
+++ Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Authentication
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """Class to manage auth
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require auth
+        """
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += "/"
+        for element in excluded_paths:
+            if element.endswith('*'):
+                if path.startswith(element[:1]):
+                    return False
+        if path in excluded_paths:
+            return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Authorization header
+        """
+        if request is None:
+            return None
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current user
+        """
+        return None
---
+++ Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+""" Basic Auth
+"""
+from api.v1.auth.auth import Auth
+import base64
+from typing import TypeVar
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """ BasicAuth class
+    """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ extract authorization header
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        basic = authorization_header.split(' ')
+        return basic[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ decode base64
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            encode = base64_authorization_header.encode('utf-8')
+            encode = base64.b64decode(encode)
+            decode = encode.decode('utf-8')
+            return decode
+        except Exception:
+            return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """ extract user credentials
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        credential = decoded_base64_authorization_header.split(":")
+        return credential[0], credential[1]
+
+    def user_object_from_credentials(self,
+                                     user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """ User object
+        """
+        if not isinstance(user_email, str) or user_email is None:
+            return None
+        if not isinstance(user_pwd, str) or user_pwd is None:
+            return None
+        try:
+            users = User.search({'email': user_email})
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        except Exception:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user
+        """
+        try:
+            header = self.authorization_header(request)
+            header64 = self.extract_base64_authorization_header(header)
+            decode = self.decode_base64_authorization_header(header64)
+            credential = self.extract_user_credentials(decode)
+            user = self.user_object_from_credentials(credential[0],
+                                                     credential[1])
+            return user
+        except Exception:
+            return None
