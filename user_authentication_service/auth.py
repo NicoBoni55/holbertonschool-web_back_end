4fce9a08cfcbc3d9982f967027e4ebd5dda76de8
NicoBoni55 <bonifrank55@gmail.com>
2025-02-26 20:33:50 -0300

    destroy session

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -76,3 +76,12 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ destroy session by user_id
+        """
+
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
