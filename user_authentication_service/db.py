02310d43e8f94fbc9c72cbd9d704041182e9afbf
NicoBoni55 <bonifrank55@gmail.com>
2025-02-26 12:17:12 -0300

    fix pycode

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""DB module
-"""
+"""DB model"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,30 +9,34 @@ from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class"""
 
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ add user
-        """
+        """add user method
 
+        Args:
+            email (str): email
+            hashed_password (str): password
+
+        Returns:
+            User: User object
+        """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
