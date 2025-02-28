b7dc6a7a2b12327e5c5affc3021909ff96463993
NicoBoni55 <bonifrank55@gmail.com>
2025-02-28 17:10:43 -0300

    update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -100,3 +100,14 @@ class Auth:
 
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            h_pww = _hash_password(password)
+            self._db.update_user(user.id,
+                                 hashed_password=h_pww, reset_token=None)
+        except NoResultFound:
+            raise ValueError
