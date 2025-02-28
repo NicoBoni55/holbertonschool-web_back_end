58d7fe954e0656dfc2f64a77d27cbc3386c5399c
NicoBoni55 <bonifrank55@gmail.com>
2025-02-28 15:59:37 -0300

    fix

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -57,7 +57,9 @@ def log_out():
 
 
 @app.route("/profile", methods=["GET"])
-def profile():
+def profile() -> str:
+    """ response to get profile
+    """
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
