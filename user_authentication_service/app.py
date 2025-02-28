d06084f6e41586d9dced3fbd2523440b11710b0b
NicoBoni55 <bonifrank55@gmail.com>
2025-02-28 16:16:56 -0300

    fix

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -68,13 +68,11 @@ def profile():
 @app.route("/reset_password", methods=["POST"])
 def get_reset_password_token():
     email = request.form.get("email")
-
-    if not email:
-        abort(403)
-    else:
+    if email:
         reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": email, "reset_token": reset_token}), 200
-
+    else:
+        abort(403)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
