738701fc97f4dd5c59d5b8af31c348cf4615116a
NicoBoni55 <bonifrank55@gmail.com>
2025-03-05 13:14:00 -0300

    fix pycode

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -3,19 +3,17 @@
 unittest
 """
 
-import unittest
-
+from unittest import TestCase
 from parameterized import parameterized
 from utils import access_nested_map
 
 
-class TestAccessNestedMap(unittest.TestCase):
+class TestAccessNestedMap(TestCase):
     """ unittest access_nested_map
     """
-
     @parameterized.expand([
         ({"a": 1}, ("a"), 1),
-        ({"a": {"b": 2}}, ("a",), 2),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
