bec68a9f50f065a54cab16d0afdab242fefc4d8d
NicoBoni55 <bonifrank55@gmail.com>
2025-03-06 20:57:54 -0300

    1-app

---
+++ i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Basic app
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def home():
+    """ basic route
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
