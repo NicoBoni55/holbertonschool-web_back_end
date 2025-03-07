18c5aa3054eb7ad1d679dd5e7fbcf01b1731c8d5
NicoBoni55 <bonifrank55@gmail.com>
2025-03-07 11:53:12 -0300

    parameter templates

---
+++ i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Basic app
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def home():
+    """ basic route
+    """
+    return render_template('3-index.html')
+
+
+def get_locale():
+    """ get locale
+    """
+    return request.accept_languages.best_match(app.config['LENGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+if __name__ == "__main__":
+    app.run()
