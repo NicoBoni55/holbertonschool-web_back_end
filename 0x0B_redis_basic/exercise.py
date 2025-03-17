ea0f0a433c00e358e6ea10525862c283a2015cd1
NicoBoni55 <bonifrank55@gmail.com>
2025-03-17 13:44:28 -0300

    redis writing strings

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Create a chache class
+"""
+
+import redis
+from typing import Union
+import uuid
+
+
+
+class Cache:
+    """
+    class Cache
+    """
+
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+    def store(self, data: Union[str, bytes, int, float]):
+        """
+        store method
+        """
+        ukey = str(uuid.uuid4())
+        self._redis.set(ukey, data)
+        return ukey
