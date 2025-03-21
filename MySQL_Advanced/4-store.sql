b02ce27a250bbedc6df9dc2ce3e27fc8f0e4fec2
NicoBoni55 <bonifrank55@gmail.com>
2025-03-21 11:32:03 -0300

    Buy buy buy

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- create trigger
+-- that decreases the quantity
+CREATE TRIGGER decreases
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name
