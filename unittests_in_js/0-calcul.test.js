c5022c1f3011bd924ebf1e2c08fc96ab71bbdaf3
NicoBoni55 <bonifrank55@gmail.com>
2025-03-24 13:46:37 -0300

    fix syntaxis in test

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -3,19 +3,21 @@ const calculateNumber = require('./0-calcul');
 
 describe('Calculate Numbers', function(){
     it('return 4, 1 + 3', function(){
-        let result = calculateNumber(1, 3)
-        assert.strictEqual(result, 4)
+        assert.strictEqual(calculateNumber(1, 3), 4)
     })
     it('return 5, 1 + 3.7', function(){
-        let result = calculateNumber(1, 3.7)
-        assert.strictEqual(result, 5)
+        assert.strictEqual(calculateNumber(1, 3.7), 5)
     })
     it('return 5, 1.2 + 3.7', function(){
-        let result = calculateNumber(1.2, 3.7)
-        assert.strictEqual(result, 5)
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5)
     })
     it('return 6, 1.5 + 3.7', function(){
-        let result = calculateNumber(1.5, 3.7)
-        assert.strictEqual(result, 6)
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6)
+    })
+    it('return -4, -1 + -3', function(){
+        assert.strictEqual(calculateNumber(-1, -3), -4)
+    })
+    it('return 0, 0 + 0', function(){
+        assert.strictEqual(calculateNumber(0, 0), 0)
     })
 });
