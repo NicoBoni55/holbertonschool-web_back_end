6170acdec79375b1e19acc50a15f39cfb3eb8867
NicoBoni55 <bonifrank55@gmail.com>
2025-03-25 10:38:54 -0300

    fix check

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -2,26 +2,13 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 const { callbackify } = require('util');
 
-describe('Calculate Numbers', function(){
-    it('return 4, 1 + 3', function(){
-        assert.strictEqual(calculateNumber(1, 3), 4)
-    })
-    it('return 5, 1 + 3.7', function(){
-        assert.strictEqual(calculateNumber(1, 3.7), 5)
-    })
-    it('return 5, 1.2 + 3.7', function(){
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5)
-    })
-    it('return 6, 1.5 + 3.7', function(){
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6)
-    })
-    it('return 5, 1.8 + 3', function(){
-        assert.strictEqual(calculateNumber(1.8, 3), 5)
-    })
-    it('return -4, -1 + -3', function(){
-        assert.strictEqual(calculateNumber(-1, -3), -4)
-    })
-    it('return 0, 0 + 0', function(){
-        assert.strictEqual(calculateNumber(0, 0), 0)
-    })
+describe('calculateNumber', () => {
+    it('checks the output', () => {
+      assert.strictEqual(calculateNumber(1, 3), 4);
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber(3.7, 1), 5);
+      assert.strictEqual(calculateNumber(3.7, 1.2), 5);
+    });
 });
