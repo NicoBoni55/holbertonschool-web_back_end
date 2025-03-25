e8c6070cdbfb90190e82de5f1aabafbf27d29ac1
NicoBoni55 <bonifrank55@gmail.com>
2025-03-25 13:02:47 -0300

    restore it

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -3,20 +3,9 @@ const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
     it('checks the output', () => {
-      it('check sum', () => {
-        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-      })
-      it('check subtract', () => {
-        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-      })
-      it('check divide', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-      })
-      it('check error', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-      })
-      it('check error', () => {
-        assert.strictEqual(calculateNumber('SUM', 1.4, 0), 1);
-      })
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
 });
