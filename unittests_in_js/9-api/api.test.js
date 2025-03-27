c173c4724c55059c01ff7f12a993e4c5dedc244c
NicoBoni55 <bonifrank55@gmail.com>
2025-03-27 12:36:13 -0300

    Regex integration testing

---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const chai = require('chai')
+
+describe('test api', () => {
+    it('status and message', (done) => {
+        const options = {
+            url: "http://localhost:7865",
+            method: "GET",
+        };
+
+        request(options, (error, response, body) => {
+            chai.expect(response.statusCode).to.equal(200);
+            chai.expect(body).to.equal('Welcome to the payment system');
+            done();
+        })
+    })
+})
+describe('test route cart/', () => {
+    it('status and message', (done) => {
+        const options = {
+            url: "http://localhost:7865/cart/12",
+            method: "GET",
+        };
+
+        request(options, (error, response, body) => {
+            chai.expect(response.statusCode).to.equal(200);
+            chai.expect(body).to.equal('Payment methods for cart 12');
+            done();
+        })
+    })
+})
